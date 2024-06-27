2f492c57065b899e7c5e52ff53b7560bdc5e533d
Anton Pashinov <troxin1988@gmail.com>
2024-06-27 11:53:04 +0700

    Add ajax for cart: add, remove, clear

--- app/carts/templatetags/carts_tags.py
+++ app/carts/templatetags/carts_tags.py
@@ -1,5 +1,6 @@
 from django import template
 
+from carts.utils import get_user_carts
 from carts.models import Cart
 
 
@@ -8,5 +9,4 @@ register = template.Library()
 
 @register.simple_tag()
 def user_carts(request):
-    if request.user.is_authenticated:
-        return Cart.objects.filter(user=request.user)
+    return get_user_carts(request)
